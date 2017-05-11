c2701774aeab70d428bdd0a3c0bc77f9d0cce3ca
kelvinabella <abellakelvin.ka@gmail.com>
2017-05-12 00:17:12 +0800

    Added dependency files

---
+++ src/server/index.js
@@ -0,0 +1,28 @@
+// @flow
+
+import compression from 'compression'
+import express from 'express'
+import { Server } from 'http'
+import socketIO from 'socket.io'
+
+import routing from './routing'
+import { WEB_PORT, STATIC_PATH, isProd } from '../shared/config'
+import setUpSocket from './socket'
+
+const app = express()
+// flow-disable-next-line
+const http = Server(app)
+const io = socketIO(http)
+setUpSocket(io)
+
+app.use(compression())
+app.use(STATIC_PATH, express.static('dist'))
+app.use(STATIC_PATH, express.static('public'))
+
+routing(app)
+
+http.listen(WEB_PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${String(WEB_PORT)} ${isProd ? '(production)' :
+    '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`)
+})
